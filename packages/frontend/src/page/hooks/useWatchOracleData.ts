811a040c360e8e25ef3632b73f6fad4363d20b2a
Paolo Guerra <pg.guerrap@proton.me>
2023-12-21 14:55:08 +0100

    Oracle fetch offload (#60)
    * chore: upgrade dependencies
    
    * feat(frontend): add hook to fetch oracle data
    
    * fix(frontend): wrong prop type name

---
+++ packages/frontend/src/page/hooks/useWatchOracleData.ts
@@ -0,0 +1,47 @@
+import { ORACLE_ABI } from "@carrot-kpi/sdk";
+import { useEffect, useState } from "react";
+import type { Hex } from "viem";
+import { type Address, useContractReads } from "wagmi";
+
+interface WatchOracleDataParams {
+    oracleAddress?: Address;
+}
+
+interface OracleData {
+    finalized: boolean;
+    data: Hex;
+}
+
+export function useWatchOracleData(
+    params?: WatchOracleDataParams,
+): OracleData | null {
+    const [data, setData] = useState<OracleData | null>(null);
+
+    const { data: readResults } = useContractReads({
+        contracts: [
+            {
+                address: params?.oracleAddress as Address | undefined,
+                abi: ORACLE_ABI,
+                functionName: "data",
+            },
+            {
+                address: params?.oracleAddress as Address | undefined,
+                abi: ORACLE_ABI,
+                functionName: "finalized",
+            },
+        ],
+        enabled: !!params?.oracleAddress,
+        watch: true,
+    });
+
+    useEffect(() => {
+        if (!readResults || !params?.oracleAddress) return;
+
+        setData({
+            data: readResults[0].result as Hex,
+            finalized: readResults[1].result as boolean,
+        });
+    }, [params?.oracleAddress, readResults]);
+
+    return data;
+}
