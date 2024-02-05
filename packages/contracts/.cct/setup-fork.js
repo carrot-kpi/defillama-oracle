0083a3120e16d0c603c8decdc7ff3a122e26f5f1
Paolo Guerra <pg.guerrap@proton.me>
2024-02-05 17:55:27 +0100

    chore(frontend): upgrade to wagmi v2

--- packages/contracts/.cct/setup-fork.js
+++ packages/contracts/.cct/setup-fork.js
@@ -39,8 +39,10 @@ export const setupFork = async ({ nodeClient, walletClient }) => {
     const tst1Contract = getContract({
         abi: erc20Abi,
         address: tst1Address,
-        publicClient: nodeClient,
-        walletClient: walletClient,
+        client: {
+            public: nodeClient,
+            wallet: walletClient,
+        },
     });
 
     const { contractAddress: tst2Address } =
@@ -54,8 +56,10 @@ export const setupFork = async ({ nodeClient, walletClient }) => {
     const tst2Contract = getContract({
         abi: erc20Abi,
         address: tst2Address,
-        publicClient: nodeClient,
-        walletClient: walletClient,
+        client: {
+            public: nodeClient,
+            wallet: walletClient,
+        },
     });
 
     // mint some test erc20 tokens to signer
